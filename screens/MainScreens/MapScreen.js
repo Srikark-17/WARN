9bf43a45f6e1eb3f6af953280aba755ee8670370
Srikark-17 <srikar.sgk@gmail.com>
2022-07-06 12:11:59 -0500

    Made a mapview button

--- screens/MainScreens/MapScreen.js
+++ screens/MainScreens/MapScreen.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
-import {
-  StyleSheet,
-  View,
-  ActivityIndicator,
-  Dimensions,
-  Text,
-} from "react-native";
+import { StyleSheet, View, Dimensions, Text } from "react-native";
 import AnimatedLoader from "react-native-animated-loader";
 import { HP, WP } from "../../config/responsive";
 
